beff0d54bb145cb328499400e786bea6b2e77ff5
Arjun Khatri <arjuncodes05@gmail.com>
2026-03-09 08:11:46 +0530

    bug fix

--- server/config/passport.js
+++ server/config/passport.js
@@ -5,18 +5,18 @@ import User from "../models/User.js";
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
   async (accessToken, refreshToken, profile, cb) => {    
     try {
-      let user = await User.findOne({ email: profile.emails[0].value});
+      let user = await User.findOne({ email: profile.emails[0]?.value});
       
       // note: if i later add "isLoggedIn" property inside my User database, make sure to upldate this findOne to findOneAndUpdate and set "isLoggedIn" to true when user logs in
       if(!user){
         user = await User.create({
           username: profile.displayName,
-          email: profile.emails[0].value,
-          image: profile.photos[0].value,
+          email: profile.emails[0]?.value,
+          image: profile.photos[0]?.value,
         })
       }
       return cb(null, user)
